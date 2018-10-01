0227e720f2c36f1449e70eeda7bd20905dd3464a
Guest User <Guest@epicodus-100.local>
2018-10-01 09:02:20 -0700

    Install redux form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import MasterListReducer from './reducer_experiences.js';
 import ActiveTrip from './reducer_selected_trip';
 
 const rootReducer = combineReducers({
   experiences: MasterListReducer,
-  activeTrip: ActiveTrip
+  activeTrip: ActiveTrip,
+  form: formReducer
 });
 
 export default rootReducer;
