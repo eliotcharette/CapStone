c9b5911679f3adf35dea0e9b8d45495a106e87f2
Eliot Charette <eliotcharette@gmail.com>
2018-10-01 16:08:18 -0700

    Clean code

--- src/index.jsx
+++ src/index.jsx
@@ -11,9 +11,9 @@ import thunkMiddleware from 'redux-thunk';
 
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
-let unsubscribe = store.subscribe(() =>
-  console.log(store.getState())
-);
+// let unsubscribe = store.subscribe(() =>
+//   console.log(store.getState())
+// );
 
 const render = (Component) => {
   ReactDOM.render(
